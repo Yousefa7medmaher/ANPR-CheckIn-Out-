222a0df13b9bdd296d4fcbfd3d31268c2fdfbc5d
JooAhmed <160799433+Yousefa7medmaher@users.noreply.github.com>
2025-03-19 08:27:32 +0200

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,13 +1,18 @@
-# db_connection.py
-
 import mysql.connector
+from mysql.connector import Error
 from config import DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE
 
 def create_db_connection():
-    db_connection = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_DATABASE
-    )
-    return db_connection
+    try:
+        db_connection = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_DATABASE
+        )
+        if db_connection.is_connected():
+            print("✅ Successfully connected to the database.")
+            return db_connection
+    except Error as e:
+        print(f"❌ Database connection failed: {e}")
+        return None
